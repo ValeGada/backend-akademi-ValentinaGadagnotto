d7ca6b681f0c8ca62fd8c5317db865f3274a095a
ValeGada <valegadagnotto@gmail.com>
2025-05-05 15:53:11 -0300

    Update check-admin.js

--- src/middlewares/check-admin.js
+++ src/middlewares/check-admin.js
@@ -3,12 +3,9 @@ const HttpError = require('../util/errors/http-error');
 
 const checkAdmin = async (req, res, next) => {
     try {
-        const user = await User.findById(decoded.userId);
-        if (user.role !== 'admin') {
-            throw new Error('You are not allowed to continue. Only admin can access.');
+        if (!req.user || req.user.role !== 'admin') {
+          throw new Error('Only admin can access.');
         }
-        
-        req.user = user;
         next();
     } catch (err) {
         return next(new HttpError('Authentication failed', 403));
